df909dc03addbfec4cd41fc762949d8ecc38a771
sabzyhacks <ei711283@gmail.com>
2026-07-06 15:16:00 +0100

    basic mvp of project completed

---
+++ script.js
@@ -0,0 +1,40 @@
+"use script";
+const changeBtn = document.querySelector(".generate");
+const colorTitle = document.querySelector("#color-tag");
+
+const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+let hexCode = "";
+let lastCode = "";
+
+const applyColor = function (code) {
+  colorTitle.innerHTML = `#${code}`;
+};
+const displayHex = function (code) {
+  document.body.style.backgroundColor = `#${code}`;
+};
+
+changeBtn.addEventListener("click", function () {
+  do {
+    hexCode = "";
+    for (let i = 0; i < 6; i++) {
+      hexCode += hexArr[Math.floor(Math.random() * hexArr.length)];
+    }
+  } while (hexCode === "" || hexCode === lastCode);
+
+  lastCode = hexCode;
+  applyColor(hexCode);
+  displayHex(hexCode);
+
+  // Contrast
+  const red = parseInt(hexCode.substring(0, 2), 16);
+  const green = parseInt(hexCode.substring(2, 4), 16);
+  const blue = parseInt(hexCode.substring(4), 16);
+  // i could have written this in one line of AVG but i want people to really understand the logic
+  const avg = (red + green + blue) / 3;
+
+  if (avg > 128) {
+    document.body.style.color = "#000000";
+  } else {
+    document.body.style.color = "#ffffff";
+  }
+});
